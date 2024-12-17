f4d71f290d55ffcba9dd040dcf134b4fc8e60eb2
leekool <lee@imre.al>
2024-12-17 18:43:29 +1100

    validate input

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { getTweet } from "./utils/apiUtils";
 
-const useDebounce = <T,>(value: T, delay: number): T => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+// const useDebounce = <T,>(value: T, delay: number): T => {
+//   const [debouncedValue, setDebouncedValue] = useState(value);
+//
+//   useEffect(() => {
+//     const handler = setTimeout(() => setDebouncedValue(value), delay);
+//     return () => clearTimeout(handler);
+//   }, [value, delay]);
+//
+//   return debouncedValue;
+// }
 
-  useEffect(() => {
-    const handler = setTimeout(() => setDebouncedValue(value), delay);
-    return () => clearTimeout(handler);
-  }, [value, delay]);
-
-  return debouncedValue;
-}
+const debounce = (fn: Function, delay: number) => {
+  let timer: number;
+  return (...args: any[]) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delay);
+  };
+};
 
 const App = () => {
   const [idValue, setIdValue] = useState("");
-  const debouncedIdValue = useDebounce(idValue, 500);
+  const [isValid, setIsValid] = useState(false);
 
   useEffect(() => {
-    console.log("test", debouncedIdValue);
-  }, [debouncedIdValue]);
+    console.log("idValue: ", idValue);
+  }, [idValue]);
 
   const click = async (id: string) => {
     const tweet = await getTweet(id);
@@ -28,16 +36,25 @@ const App = () => {
 
   const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIdValue(e.target.value);
+    validateInput(e.target.value);
   }
+
+  const validateInput = useRef(
+    debounce((input: string) => {
+      const match = input.match(/\b\d{17,20}\b/);
+      setIsValid(!!match);
+    }, 500)
+  ).current;
+
 
   return (
     <>
       <div>
         <input type="text" value={idValue} onChange={handleIdChange} />
         <button
-          onClick={() => click(debouncedIdValue)}
+          onClick={() => click(idValue)}
         >
-          test
+          {isValid ? "valid" : "invalid"}
         </button>
       </div>
     </>
