be56340e678af8d4de3f57d812c4a80f4e32e0dd
leekool <lee@imre.al>
2024-12-17 18:16:14 +1100

    debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { getTweet } from "./utils/apiUtils";
 
-function App() {
-  const [idValue, setIdValue] = useState("");
+const useDebounce = <T,>(value: T, delay: number): T => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIdValue(e.target.value);
-  }
+  useEffect(() => {
+    const handler = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+const App = () => {
+  const [idValue, setIdValue] = useState("");
+  const debouncedIdValue = useDebounce(idValue, 500);
+
+  useEffect(() => {
+    console.log("test", debouncedIdValue);
+  }, [debouncedIdValue]);
 
   const click = async (id: string) => {
     const tweet = await getTweet(id);
     console.log(tweet);
+  }
+
+  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIdValue(e.target.value);
   }
 
   return (
@@ -19,7 +35,7 @@ function App() {
       <div>
         <input type="text" value={idValue} onChange={handleIdChange} />
         <button
-          onClick={() => click(idValue)}
+          onClick={() => click(debouncedIdValue)}
         >
           test
         </button>
